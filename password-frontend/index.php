ced8c7ee6a5ee11475f233177d59ce873d50410d
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-09-13 03:38:21 +0300

    password-frontend

---
+++ password-frontend/index.php
@@ -0,0 +1,30 @@
+<?php
+
+if (isset($_POST['submit'])){
+    $password =$_POST['password'];
+}
+
+$length=strlen($password);
+$uppercase = preg_match('@[A-Z]@', $password);
+$lowercase = preg_match('@[a-z]@', $password);
+$number    = preg_match('@[0-9]@', $password);
+$specialChars = preg_match('@[^\w]@', $password);
+
+$strong=' ';
+
+if($length <12 || !$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+    $strong='no';
+}
+
+else{
+    $strong='yes';
+}
+
+$result = [
+    'password' => $password,
+    'strong' => $strong
+];
+
+echo json_encode($result);
+
+?>
